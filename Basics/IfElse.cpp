5a6cd5a0e58ce3d1eeed209d690465f7e3f80529
SmartKushi1 <117197578+SmartKushi1@users.noreply.github.com>
2023-06-25 22:02:15 +0530

    Create IfElse.cpp
    ADDED PROGRAM

---
+++ Basics/IfElse.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string gender;
+    cin>> gender;
+    
+    if(gender == "boy"){
+        cout<<"im a boy";
+    }
+    else{
+        cout<<"im a girl";
+    }
+
+    return 0;
+}
