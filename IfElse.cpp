e5906c06524381e7a55f9d944acb620c442e847f
SmartKushi1 <117197578+SmartKushi1@users.noreply.github.com>
2023-06-30 02:36:47 +0530

    Create IfElse.cpp
    add a file

---
+++ IfElse.cpp
@@ -0,0 +1,19 @@
+int main()
+{
+    int a;
+    cout<<"enter a number";
+    cin>>a;
+
+
+    if(a%5==0&&a%3==0){
+    cout<<"it is divisiable byi both";
+    }else if(a%5==0){
+        cout<<"by 5";
+    }else if(a%3==0){
+        cout<<"by 3";
+    }else{
+    cout<<"it is not divisable";
+    }
+    
+      
+}
